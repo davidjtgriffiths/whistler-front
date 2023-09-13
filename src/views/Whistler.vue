08be69df53c3a4b00b89fc628b400a2733720b74
dave <davidgriffiths@hotmail.com>
2023-09-13 11:18:24 +0100

    with refs passed from form

--- src/views/Whistler.vue
+++ src/views/Whistler.vue
@@ -1,41 +1,46 @@
 <template>
-  <div class="flex flex-wrap mt-4">
-    <div class="w-full mb-12 px-4">
-      <!-- Small Input -->
-      <div class="mb-3 pt-0">
-        <input type="text" placeholder="Small Input" class="px-2 py-1 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"/>
-      </div>
-      <!-- Regular Input -->
-      <div class="mb-3 pt-0">
-        <input type="text" placeholder="Regular Input" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full"/>
-      </div>
-      <!-- Large Input -->
-      <div class="mb-3 pt-0">
-        <input type="text" placeholder="Large Input" class="px-3 py-4 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-base shadow outline-none focus:outline-none focus:shadow-outline w-full"/>
-      </div>
-      <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-        Small
-      </button>
-      <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-        Regular
-      </button>
-      <button
-      @click.prevent="
-                                        onSubmit()
-                                    "
-        class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-        Large
-      </button>
-    </div>
-    <div class="w-full mb-12 px-4">
-      <card-table color="dark" />
+<div>
+    <!-- Your form goes here -->
+    <form @submit.prevent="submitForm">
+      <label for="title">Title:</label>
+      <input type="text" id="title" v-model="postTitle" />
+
+      <label for="content">Content:</label>
+      <textarea id="content" v-model="postContent"></textarea>
+
+      <button type="submit">Submit</button>
+    </form>
+
+    <!-- Display the response -->
+    <div v-if="response">
+      <h2>Response from Server:</h2>
+      <pre>{{ response }}</pre>
     </div>
   </div>
 </template>
 <script setup>
 /* eslint-disable */
-import axios from "axios";
-import CardTable from "@/components/Cards/CardTable.vue";
+import { ref } from 'vue';
+import axios from 'axios';
+
+const postTitle = ref('');
+const postContent = ref('');
+const response = ref(null);
+
+const submitForm = async () => {
+  try {
+    const responseData = await axios.post('https://example.com/api/posts', {
+      title: postTitle.value,
+      content: postContent.value,
+    });
+
+    // Handle the response as needed
+    response.value = responseData.data;
+  } catch (error) {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  }
+};
 
 
   const onSubmit = async () => {
